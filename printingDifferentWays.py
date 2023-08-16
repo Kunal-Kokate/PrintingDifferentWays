992f2ee01cbefee94d4459289960d7b494b876fd
Kunal <79614769+Kunal-Kokate@users.noreply.github.com>
2023-08-16 18:50:51 -0400

    Add files via upload

---
+++ printingDifferentWays.py
@@ -0,0 +1,12 @@
+#There are different ways to print stuff in python
+
+youtuber = 'Codingsolo'
+
+# This one we concatanate it together
+print("Subscribe to " + youtuber)
+
+#Puts whatever the youtuber is into the brackets
+print("Subscribe to {}".format(youtuber))
+
+#Easiest method. Just put f then put the variable name in the {}
+print(f"Subscribe to {youtuber}")
